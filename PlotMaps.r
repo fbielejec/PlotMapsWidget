9c42338625881000d82ae188422ada00c1975a9a
fbielejec <fbielejec@gmail.com>
2010-11-16 12:50:48 +0100

    Run file

---
+++ PlotMaps.r
@@ -0,0 +1,22 @@
+############
+#---TODO---#
+############
+# Make it work in two modes: partial data, full globe
+# Do sth about bad text labelling (centers)
+# Add more options (font size, vertical, horizontal adjust)
+# Rate labels in proper order 
+
+
+###########
+#---RUN---#
+###########
+#adjust path here:
+#setwd("C:\\Documents and Settings\\filip\\My Documents\\My Dropbox\\Phyleography\\PlotMaps")
+setwd("/home/filip/Dropbox/Phyleography/PlotMaps")
+
+source("PlotMaps_libs.r")
+source("PlotMaps_ggplotUtils.r")
+source("PlotMaps_GUI.r")
+
+
+
