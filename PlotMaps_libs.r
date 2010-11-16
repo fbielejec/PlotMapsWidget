fc85ca595b48d6354f70713374f2f3923bc1456b
fbielejec <fbielejec@gmail.com>
2010-11-16 12:50:49 +0100

    libraries

---
+++ PlotMaps_libs.r
@@ -0,0 +1,13 @@
+#################
+#---LIBRARIES---#
+#################
+library(ggplot2)
+library(maps)
+library(maptools)
+library(rgdal)
+library(gWidgets)
+options(guiToolkit = "RGtk2")
+library(gWidgetsRGtk2)
+
+#options(guiToolkit = "tcltk")
+#library(gWidgetstcltk)
