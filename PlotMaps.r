78ab51b538c726cb459ce95647a057f4f83f250b
fbielejec <fbielejec@gmail.com>
2010-11-17 13:41:04 +0100

    Run file

--- PlotMaps.r
+++ PlotMaps.r
@@ -2,11 +2,10 @@
 #---TODO---#
 ############
 # Make it work in two modes: partial data, full globe
-# Do sth about bad text labelling (centers)
 # Add more options (font size, vertical, horizontal adjust)
 # Supress all the warnings from rgdal::readOGR 
-# parse the data and labels of the locations from kml file only
 # add some jitter to labels positions to avoid overplotting
+# add more tryCatch blocks, especially for plotting
 
 ###########
 #---RUN---#
