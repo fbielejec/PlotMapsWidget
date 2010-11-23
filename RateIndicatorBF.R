a957f82e9ac91092e734f33e237a3ed110c888c5
fbielejec <fbielejec@gmail.com>
2010-11-23 16:58:24 +0100

    BF test

---
+++ RateIndicatorBF.R
@@ -0,0 +1,103 @@
+############
+#---TEST---#
+############
+# /home/filip/Dropbox/Phyleography/PlotMaps/supplementary/Test/indicators.log
+# /home/filip/Dropbox/Phyleography/PlotMaps/supplementary/Test/locationDegrees
+#
+#################
+##---Philippe---#
+#################
+# /home/filip/Dropbox/Phyleography/PlotMaps/supplementary/Philippe/genomes.HKYG.UCLN.EGC.DISC.BSSVS.Indicator.log
+# /home/filip/Dropbox/Phyleography/PlotMaps/supplementary/Philippe/locationDegrees
+#
+##############
+##---Nuno---#
+##############
+# /home/filip/Dropbox/Phyleography/PlotMaps/supplementary/Nuno/HIV2A_WAcombi_equalfreq_bssvs_rateMatrix.log
+# /home/filip/Dropbox/Phyleography/PlotMaps/supplementary/Nuno/locationHIV2A.txt
+
+####################
+#---PROGRAM FLOW---#
+####################
+RateIndicatorBF <- function(h,...){
+
+	   tryCatch({
+				   
+svalue(status_bar) <- "Computing..."
+loc                <- read.table(svalue(LocFile), head=FALSE)
+Ind                <- read.table(svalue(LogFile), head=TRUE)
+
+# hard-coded for now
+BurnIn    = 0.1
+BFcutoff  = svalue(SpecifyBFCutoff)
+
+Ind <- Ind[grep("indicators", names(Ind))]
+delete <- round(dim(Ind)[1] * BurnIn)
+Ind    <- Ind[-c(1:delete),]
+
+
+K <- dim(loc)[1]
+if( ncol(Ind)==K*(K-1) ) {
+	symmetrical <- FALSE
+} else if (ncol(Ind)==(K*(K-1))/2) {
+	symmetrical <- TRUE
+} else {
+	cat("the number of rate indicators does not match the number of locations! \n")
+}
+
+
+variables    <- as.character(loc[1]$V1)
+combinations <- combn(variables, 2)
+combinations <- paste(combinations[1,], combinations[2,], sep=":" )
+
+# recognise here, paste accordingly 1 or 2
+ColNames <- switch(as.character(symmetrical),
+		"TRUE" =  combinations,
+		"FALSE" = rep(combinations, 2)
+)	
+names(Ind) <- ColNames
+
+
+# divide by 1 or 2 accordingly
+numberOfRatesMultiplier <- switch(as.character(symmetrical),
+		"TRUE" =  2,
+		"FALSE" = 1
+)	
+
+
+qk <- (log(2) + K - 1) / ( (K*(K-1)) / numberOfRatesMultiplier )
+pk <- apply(Ind, 2, mean)
+
+out <- (pk/(1-pk)) / (qk/(1-qk))
+
+data <- data.frame(
+I = pk[which(out > BFcutoff)],
+BF = out[which(out > BFcutoff)],
+from = do.call("rbind", strsplit(names(out[which(out > BFcutoff)]), ":"))[,1],
+to = do.call("rbind", strsplit(names(out[which(out > BFcutoff)]), ":"))[,2]
+)
+data$x = loc$V3[ match(data$from, loc$V1)  ]
+data$y = loc$V2[ match(data$from, loc$V1)  ]
+data$xend = loc$V3[ match(data$to, loc$V1)  ]
+data$yend = loc$V2[ match(data$to, loc$V1)  ]
+data$I <- round(data$I, 2)
+data$BF <- round(data$BF, 2)
+data$I <- as.factor(data$I)
+data$BF <- as.factor(data$BF)
+row.names(data) <- NULL
+
+locations <- data.frame(
+location = with(data, c(as.character(from), as.character(to) ) ),
+Longitude = with(data, c(x, xend) ),
+Latitude = with(data, c(y, yend) )
+)
+locations <- unique(locations)
+
+assign("out", data, envir = globalenv())
+assign("locations", locations, envir = globalenv())
+
+svalue(status_bar) <- "Done."
+
+    }, error = function(e) svalue(status_bar) <- "Could not compute!")
+
+} #END: RateIndicatorBF
