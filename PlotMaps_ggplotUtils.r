ebc6484d2a45f7601772cd08b4bf56486129c4ed
fbielejec <fbielejec@gmail.com>
2010-11-24 13:03:54 +0100

    added plot on full globe functionality

--- PlotMaps_ggplotUtils.r
+++ PlotMaps_ggplotUtils.r
@@ -27,8 +27,8 @@ PlotOnMap <- function(h,...) {
 	
 	keepMap <- (world.map$lat >= MinLat - offest) & (world.map$lat <= MaxLat + offest) & (world.map$long >= MinLon - offest) & (world.map$long <= MaxLon + offest)
 	
-	mode <- svalue(select_coord)
-	MapData <- switch(mode,
+	coord_mode <- svalue(select_coord)
+	MapData <- switch(coord_mode,
 			"map" = world.map[keepMap,],
 			"cartesian" =  world.map[keepMap,],
 			"full globe" = world.map
@@ -41,7 +41,7 @@ PlotOnMap <- function(h,...) {
 	p.map <- ggplot(MapData, aes(long, lat)) 
 	p.map <- p.map + geom_polygon(aes(long, lat, group = group ), fill = I(poly_color), size = .2, color = I(boundaries_color)) 
 	
-	p.map <- switch(mode,
+	p.map <- switch(coord_mode,
 			"map" = p.map + coord_map(projection = "tetra", xlim = c(MinLon, MaxLon + MAXSTRING), ylim = c(MinLat, MaxLat)),
 			"cartesian" =  p.map + coord_cartesian(xlim = c(MinLon, MaxLon + MAXSTRING), ylim = c(MinLat, MaxLat)),
 			# map coordinates cause problem:
@@ -51,23 +51,28 @@ PlotOnMap <- function(h,...) {
 
 	
 	p.map <- p.map + geom_point(data = locations, aes(x = Longitude, y = Latitude), color = I("white"), size = locations_size)	
+	
 	# supress arrows at value 0
-	if(svalue(arrow_size)!=0) {
-		p.map <- p.map + geom_segment(data = out, aes(x = x, y = y, xend = xend, yend = yend, color = BF ), size = I(arrow_size), arrow = arrow(length = unit(0.25,"cm"), ends="last" ) ) 
+	if(svalue(arrow_size)!=0) {		
+		arrow_mode <- svalue(select_arrow_tips)
+		p.map <- switch(arrow_mode, 
+				"last"  = p.map + geom_segment(data = out, aes(x = x, y = y, xend = xend, yend = yend, color = BF ), size = I(arrow_size), arrow = arrow(length = unit(0.25,"cm"), ends="last" ) ), 
+				"first" = p.map + geom_segment(data = out, aes(x = x, y = y, xend = xend, yend = yend, color = BF ), size = I(arrow_size), arrow = arrow(length = unit(0.25,"cm"), ends="first" ) ),
+				"both"  = p.map + geom_segment(data = out, aes(x = x, y = y, xend = xend, yend = yend, color = BF ), size = I(arrow_size), arrow = arrow(length = unit(0.25,"cm"), ends="both" ) ), 
+				"none"  = p.map + geom_segment(data = out, aes(x = x, y = y, xend = xend, yend = yend, color = BF ), size = I(arrow_size) )
+		)
 	}
 	p.map <- p.map + geom_text(data = locations, aes(x = Longitude, y = jitter(Latitude, 35), label = location), hjust = -0.1, family = 3, vjust = 0.0, size = svalue(text_labels_size), color = text_labels_col) 
 
 	
-	if(mode %in% c("map", "cartesian") ) {
-	
+	if(coord_mode %in% c("map", "cartesian") ) {	
 		xgrid <- grid.pretty(c(MaxLon, MinLon)) 
 		xmaj  <- xgrid[-length(xgrid)]
 		ygrid <- grid.pretty(c(MaxLat, MinLat)) 
 		ymaj  <- ygrid[-length(ygrid)]
 }
 	
-	if(mode == "full globe") {
-	
+	if(coord_mode == "full globe") {	
 		theme_null <- theme_update(
 			panel.grid.major = theme_blank(),
 			panel.grid.minor = theme_blank()
@@ -75,7 +80,7 @@ PlotOnMap <- function(h,...) {
 }
 
 
-	p.map <- switch(mode,
+	p.map <- switch(coord_mode,
 			"map" = p.map + scale_y_continuous(breaks = ymaj ) + scale_x_continuous(breaks = xmaj ),
 			"cartesian" = p.map + scale_y_continuous(breaks = ymaj ) + scale_x_continuous(breaks = xmaj ),
 			"full globe" = p.map + theme_set(theme_null)
