76638c60e40b7e814b261d5f1dfbfa620845a132
fbielejec <fbielejec@gmail.com>
2010-11-24 15:00:19 +0100

    libraries

--- PlotMaps_libs.r
+++ PlotMaps_libs.r
@@ -1,13 +1,13 @@
 #################
 #---LIBRARIES---#
 #################
-library(combinat)
 library(ggplot2)
 library(maps)
 library(cairoDevice) 
 library(gWidgets)
 options(guiToolkit = "RGtk2")
 library(gWidgetsRGtk2)
+#library(svSocket)
 
 #options(guiToolkit = "tcltk")
 #library(gWidgetstcltk)
