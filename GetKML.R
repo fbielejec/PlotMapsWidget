2bc33aff9a72cb21db2fb349a520fa310476e347
fbielejec <fbielejec@gmail.com>
2010-11-26 14:50:21 +0100

    utilities to generate kml output

---
+++ GetKML.R
@@ -0,0 +1,21 @@
+RowAsMatrix <- function(lines, row) {	
+	rbind(c(lines[row,]$x, lines[row,]$y), c(lines[row,]$xend, lines[row,]$yend) )
+}
+
+GetKML <- function(h,...) {
+
+#	tryCatch({
+	
+			lines <- out[5:8]
+			lines_list <- list()
+				
+	for(i in 1 : dim(lines)[1] ) {	
+		lines_list[[i]] <- Line( RowAsMatrix(lines, i) )
+	}
+
+lines <- Lines(lines_list, ID = "BF")
+kmlLine(lines, kmlfile = h$file)
+svalue(status_bar) <- "Done."	
+#     }, error = function(e) svalue(status_bar) <- "Could not save!")
+
+}
